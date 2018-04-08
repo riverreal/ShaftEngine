fff917de1a317ddb9dadfec315fb401851e74d70
YuFukai201 <yu.fukai0201@gmail.com>
2018-04-08 14:37:02 +0900

    Add ModelBuilder

---
+++ Source/Engine/Graphics/ModelBuilder.cpp
@@ -0,0 +1,10 @@
+#include"ModelBuilder.h"
+
+using namespace Shaft;
+
+MeshData Shaft::ModelBuilder::CreateModel(std::string fileName)
+{
+	MeshData mesh;
+
+	return mesh;
+}
---
+++ Source/Engine/Graphics/ModelBuilder.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../System/Misc/Vertex.h"
+
+namespace Shaft
+{
+	class ModelBuilder
+	{
+	public:
+		static MeshData CreateModel(std::string fileName);
+
+	private:
+
+	};
+}
